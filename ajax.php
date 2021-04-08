acf335ddb51ab1fc7ea3576d0d243af773288c42
mac <mac@Roman-Timoshchuk.local>
2021-04-08 13:16:16 +0300

    telegram sender

---
+++ ajax.php
@@ -0,0 +1,51 @@
+<?php
+// Токен телеграм бота
+$tg_bot_token = "1762292099:AAH8kXjoMLYYuXURg93z9Xu7dX_ysNnGlco";
+// ID Чата
+$chat_id = "-229040258";
+
+$text = '';
+
+foreach ($_POST as $key => $val) {
+    $text .= $key . ": " . $val . "\n";
+}
+
+$text .= "\n" . $_SERVER['REMOTE_ADDR'];
+$text .= "\n" . date('d.m.y H:i:s');
+
+$param = [
+    "chat_id" => $chat_id,
+    "text" => $text
+];
+
+$url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendMessage?" . http_build_query($param);
+
+var_dump($text);
+
+file_get_contents($url);
+
+foreach ( $_FILES as $file ) {
+
+    $url = "https://api.telegram.org/bot" . $tg_bot_token . "/sendDocument";
+
+    move_uploaded_file($file['tmp_name'], $file['name']);
+
+    $document = new \CURLFile($file['name']);
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, ["chat_id" => $chat_id, "document" => $document]);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    $out = curl_exec($ch);
+
+    curl_close($ch);
+
+    unlink($file['name']);
+}
+
+die('1');
---
+++ main.js
@@ -0,0 +1,66 @@
+$('.telegram-form').on('submit', function (event) {
+
+    event.stopPropagation();
+    event.preventDefault();
+
+    let form = this,
+        submit = $('.submit', form),
+        data = new FormData(),
+        files = $('input[type=file]')
+
+
+    $('.submit', form).val('Отправка...');
+    $('input, textarea', form).attr('disabled','');
+
+    data.append( 'name', 		$('[name="name"]', form).val() );
+    data.append( 'phone', 		$('[name="phone"]', form).val() );
+    data.append( 'email', 		$('[name="email"]', form).val() );
+    data.append( 'text', 		$('[name="text"]', form).val() );
+    data.append( 'file', 		$('[name="file"]', form).val() );
+   
+
+    files.each(function (key, file) {
+        let cont = file.files;
+        if ( cont ) {
+            $.each( cont, function( key, value ) {
+                data.append( key, value );
+            });
+        }
+    });
+    
+    $.ajax({
+        url: 'ajax.php',
+        type: 'POST',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        xhr: function() {
+            let myXhr = $.ajaxSettings.xhr();
+
+            if ( myXhr.upload ) {
+                myXhr.upload.addEventListener( 'progress', function(e) {
+                    if ( e.lengthComputable ) {
+                        let percentage = ( e.loaded / e.total ) * 100;
+                            percentage = percentage.toFixed(0);
+                        $('.submit', form)
+                            .html( percentage + '%' );
+                    }
+                }, false );
+            }
+
+            return myXhr;
+        },
+        error: function( jqXHR, textStatus ) {
+            // Тут выводим ошибку
+        },
+        complete: function() {
+            // Тут можем что-то делать ПОСЛЕ успешной отправки формы
+            console.log('Complete')
+            form.reset() 
+        }
+    });
+
+    return false;
+});
